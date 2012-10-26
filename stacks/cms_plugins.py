3786fed1d57b6f3d214c2ce5d09676c1959e582c
Patrick Lauber <digi@treepy.com>
2012-10-26 18:31:46 +0200

    stack plugin working

--- stacks/cms_plugins.py
+++ stacks/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
 from stacks.models import StackLink
+from cms.plugins.utils import get_plugins
 
 class StackPlugin(CMSPluginBase):
     model = StackLink
@@ -10,10 +11,11 @@ class StackPlugin(CMSPluginBase):
     render_template = "cms/plugins/stacks.html"
 
     def render(self, context, instance, placeholder):
-
+        plugins = get_plugins(context['request'], instance.stack.content)
         context.update({
             'object': instance,
             'placeholder':placeholder,
+            'plugins':plugins,
         })
         return context
 
