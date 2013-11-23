098a165a0924fa1f965505515cba78bdc4d3417a
Iacopo Spalletti <i.spalletti@nephila.it>
2013-11-23 22:45:04 +0100

    Add tests for unicode paths #1418

--- cms/tests/urlutils.py
+++ cms/tests/urlutils.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement
+from django.core.urlresolvers import resolve
+
+from cms.api import create_page
 from cms.test_utils.testcases import CMSTestCase
 from cms.test_utils.util.context_managers import SettingsOverride
 from cms.utils import urlutils
+from cms.utils.page_resolver import get_page_from_request
+from cms.views import details
 
 
 class UrlutilsTestCase(CMSTestCase):
@@ -15,6 +20,33 @@ class UrlutilsTestCase(CMSTestCase):
         self.assertEqual('a/b/c/', urlutils.urljoin('a', 'b', 'c'))
         self.assertEqual('a/b/c/', urlutils.urljoin('a', '//b//', 'c'))
         self.assertEqual('a/', urlutils.urljoin('a', ''))
+
+    def test_url_regexp(self):
+        """
+        Tests for correct urlconf resolution for plain ASCII and Unicode slugs
+        """
+        slug1 = u'test-page'
+        slug2 = u'unicodè'
+        home = create_page('home', 'nav_playground.html', 'en',
+                           published=True)
+        page1 = create_page('test page', 'nav_playground.html', 'en',
+                            slug=slug1, published=True, parent=home)
+        page2 = create_page(u'unicode page', 'nav_playground.html', 'en',
+                            slug=slug2, published=True, parent=home)
+
+        testurl1 = u'/en/%s/' % slug1
+        resolved = resolve(testurl1)
+        page = get_page_from_request(self.get_request(testurl1))
+        self.assertEqual(resolved.func, details)
+        self.assertEqual(resolved.kwargs['slug'], slug1)
+        self.assertEqual(page.get_path(), slug1)
+
+        testurl2 = u'/en/%s/' % slug2
+        resolved = resolve(testurl2)
+        page = get_page_from_request(self.get_request(testurl2))
+        self.assertEqual(resolved.func, details)
+        self.assertEqual(resolved.kwargs['slug'], slug2)
+        self.assertEqual(page.get_path(), slug2)
 
     def test_is_media_url(self):
         with SettingsOverride(MEDIA_URL='/media/'):
